5f72e09c2afef42d0232b4503302b8cdc0fc61e6
Aji Achmad Mustofa <micro280991@gmail.com>
2025-09-05 05:42:42 +0700

    Prepare model user for following mechanism

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,14 @@
 class User < ApplicationRecord
   has_many :sleep_trackers, dependent: :destroy
+  has_many :active_relationships, class_name: "Follow",
+                                  foreign_key: "follower_id",
+                                  dependent: :destroy
+  has_many :followings, through: :active_relationships, source: :followed
+
+  has_many :passive_relationships, class_name: "Follow",
+                                   foreign_key: "followed_id",
+                                   dependent: :destroy
+  has_many :followers, through: :passive_relationships, source: :follower
 
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,4 +10,19 @@ RSpec.describe User, type: :model do
     user = build(:user, email: nil)
     expect(user).not_to be_valid
   end
+
+  describe "follower and following associations" do
+    let(:user) { create(:user) }
+    let(:other_user) { create(:user) }
+
+    it "can follow another user" do
+      user.followings << other_user
+      expect(user.followings).to include(other_user)
+    end
+
+    it "can be followed by another user" do
+      other_user.followings << user
+      expect(user.followers).to include(other_user)
+    end
+  end
 end
