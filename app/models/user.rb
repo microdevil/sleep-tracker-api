a943bcacf5d3407a0218bf723bde9d24b4de5bc4
Aji Achmad Mustofa <micro280991@gmail.com>
2025-09-05 05:52:31 +0700

    Fix setup devise as authorization controll

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :sleep_trackers, dependent: :destroy
   has_many :active_relationships, class_name: "Follow",
                                   foreign_key: "follower_id",
